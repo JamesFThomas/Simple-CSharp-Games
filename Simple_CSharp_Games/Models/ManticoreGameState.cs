61d6955f0a354e57faef6569f9a252f5ebf7f8b0
James Thomas <jamesfeltonthomas@gmail.com>
2025-07-10 13:10:45 -0500

    Hunting The Manticore: Basic UI working and restarting - no shots history

--- Simple_CSharp_Games/Models/ManticoreGameState.cs
+++ Simple_CSharp_Games/Models/ManticoreGameState.cs
@@ -4,30 +4,118 @@ namespace Simple_CSharp_Games.Models
     public class ManticoreGameState
     {
         public int Round { get; set; } = 1; 
+
+        public List<(int Guess, string Result)> Shots { get; set; } = new List<(int Guess, string Result)>();
+
+        public Manticore Manticore { get; set; } = new Manticore();
+
+        public City City { get; set; } = new City();
+
+        public bool IsGameOver => Manticore.Health <= 0 || City.Health <= 0;
+
+        public string? Winner { get; set; }
+
         public ManticoreGameState() { }
+
+        public void TakeTurn(int guess)
+        {
+            CheckShotResult(guess);
+            AttackCity();
+            CheckGameOver();
+        }
+
+        public void CheckGameOver()
+        {
+            if (Manticore.Health <= 0)
+            {
+                Winner = "City";
+            }
+            else if (City.Health <= 0)
+            {
+                Winner = "Manticore";
+            }
+            else
+            {
+                IncreaseRound();
+            }
+        }
+
+        public void CheckShotResult(int guess)
+        {
+            if (guess < Manticore.Range)
+            {
+                Shots.Add((guess, "Short"));
+            }
+            else if (guess > Manticore.Range)
+            {
+                Shots.Add((guess, "Long"));
+            }
+            else
+            {
+                Shots.Add((guess, "Hit"));
+                City.FireMagicCannon(Round, Manticore);
+            }
+        }
+
+        public void AttackCity()
+        {
+            if (Manticore.Health > 0)
+            {
+                Manticore.AttackCity(City);
+            }
+        }
+
+        public void IncreaseRound()
+        {
+            Round++;
+        }
+
+        public void ResetGame()
+        {
+            Round = 1;
+            Shots.Clear();
+            Manticore = new Manticore();
+            City = new City();
+            Winner = null;
+        }
 
     }
 
     public class Manticore 
     {
         public int Health { get; set; } = 15;
-        public int Range { get; set; } = 0;
+        public int Range { get; set; }
 
         private static readonly Random _random = new Random();
 
-        public Manticore() { }
-
-        public void SetManticoreRange()
-        {
+        public Manticore() 
+        { 
             Range = _random.Next(0, 101);
+        }
+
+
+        public void AttackCity(City city)
+        {
+            if (city.Health > 0)
+            {
+                city.Health -= 1;
+            }
         }
     }
 
     public class  City
     {
-        public int Health { get; set; } = 10;
+        public int Health { get; set; } = 5;
 
         public City() { }
+
+        public void FireMagicCannon(int round, Manticore manticore)
+        {
+            MagicCannon cannon = new MagicCannon();
+            int damage = cannon.CalculateDamage(round);
+            manticore.Health -= damage;
+
+        }
     }
 
     public class MagicCannon
